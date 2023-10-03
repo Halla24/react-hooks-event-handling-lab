1f59a18524713fff6026e99ca832cc4d0695828a
halla <aniabisso.16@gmail.com>
2023-10-03 20:38:12 +0100

    completed assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,24 @@
-// Code EyesOnMe Component Here
+// components/EyesOnMe.js
+import React, { Component } from 'react';
+
+class EyesOnMe extends Component {
+  // Event handlers for focus and blur events
+  handleFocus = () => {
+    console.log('Good!');
+  }
+
+  handleBlur = () => {
+    console.log('Hey! Eyes on me!');
+  }
+
+  render() {
+    return (
+      <button onFocus={this.handleFocus} onBlur={this.handleBlur}>
+        Eyes on me
+      </button>
+    );
+  }
+}
+
+export default EyesOnMe;
+
